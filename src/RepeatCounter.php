9abe1b380deaf176e81f3448e1500e9b309241bf
Jonathan Lin <johnlin0214@gmail.com>
2015-03-13 13:27:19 -0400

    Add code to pass second test -foreach loop

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -2,8 +2,12 @@
     class RepeatCounter {
         function countRepeats($input_search, $input_sentence) {
             $amount_of_appereances = 0;
-            if ($input_search == $input_sentence) {
-                $amount_of_appereances += 1;
+            $sentence_array = explode( " ", $input_sentence);
+
+            foreach ($sentence_array as $word_in_sentence) {
+                if ($input_search == $word_in_sentence) {
+                    $amount_of_appereances += 1;
+                }
             }
             return $amount_of_appereances;
         }
