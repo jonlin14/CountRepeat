7ef2aeeee841a22e45b298a2c3f2988e280112f3
Jonathan Lin <johnlin0214@gmail.com>
2015-03-13 16:25:14 -0700

    Add indentation fix to RepeatCounter.php and added comments

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -29,8 +29,8 @@
 
             $sentence_array = explode( " ", $this->getSentence());
 
-
             foreach ($sentence_array as $word_in_sentence) {
+
                 if ((ctype_alnum($word_in_sentence)) != 1) {
                     $word_in_sentence = substr_replace($word_in_sentence,"",-1);
                     if ($this->getWord() == $word_in_sentence) {
@@ -47,5 +47,10 @@
 
         }
     }
-
+/*
+ I don't think that I need the Amount_of_appearances to be a property of the class but I just added
+ it in there because I was trying to debug something and was just trying everything to try and fix it.
+ I do think that it will allow me to show multiple word searches on the same page because everything needed
+ to do the search is contained in that specific instance of the object.
+*/
 ?>
