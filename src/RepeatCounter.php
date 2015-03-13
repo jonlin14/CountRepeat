093c14168eeb9f1618c0b521111ca767119b8fc6
Jonathan Lin <johnlin0214@gmail.com>
2015-03-13 13:18:34 -0400

    Add code to pass first test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,7 +1,11 @@
 <?php
     class RepeatCounter {
-        function countRepeats() {
-
+        function countRepeats($input_search, $input_sentence) {
+            $amount_of_appereances = 0;
+            if ($input_search == $input_sentence) {
+                $amount_of_appereances += 1;
+            }
+            return $amount_of_appereances;
         }
     }
 
